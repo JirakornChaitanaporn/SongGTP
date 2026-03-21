7897849a54f3892944ec1fb1516605c7c5d6b9e5
Jirakorn Chaitanaporn <Jirakorn.c@ku.th>
2026-03-21 20:46:18 +0700

    Fix: remove nested app folders, keep clean root structure

--- songGTP/library/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from user.models import User
-
-# Create your models here.
-class Library(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE, related_name="library_user_id")
-    created_at = models.DateTimeField(auto_now_add=True)
